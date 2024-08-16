a94cca0a2c218cbf46a158cb9c0be90f1ec5b9c6
Wren Durbano <wren.durbano@gmail.com>
2024-08-16 08:46:32 -0600

    Remove example code

--- game.js
+++ game.js
@@ -1,38 +1,15 @@
-// title:   game title
-// author:  game developer, email, etc.
-// desc:    short description
-// site:    website link
-// license: MIT License (change this to your license of choice)
+// title:   GMTK 2024
+// authors: Blessed BlessedEgg, Comet1772, lemgob, Wren
+// desc:    
+// site:    
+// license: MIT License
 // version: 0.1
 // script:  js
 
-var t=0
-var x=96
-var y=24
-
-function TIC()
-{
-	if(btn(0))y--
-	if(btn(1))y++
-	if(btn(2))x--
-	if(btn(3))x++
-
+function TIC() {
 	cls(13)
-	spr(1+((t%60)/30|0)*2,x,y,14,3,0,0,2,2)
 	print("HELLO WORLD!",84,84)
-	t++
 }
-
-// <TILES>
-// 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
-// 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
-// 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
-// 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
-// 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
-// 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
-// 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
-// 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
-// </TILES>
 
 // <WAVES>
 // 000:00000000ffffffff00000000ffffffff
