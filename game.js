2eb6dfcc4bceece47404d3969f2111e088ddfbdd
Wren Durbano <wren.durbano@gmail.com>
2024-08-17 10:14:35 -0600

    Add player movement

--- game.js
+++ game.js
@@ -6,9 +6,164 @@
 // version: 0.1
 // script:  js
 
+let player;
+let frame = 0;
+
+function BOOT() {
+	player = new Player();
+	player.setPosition(new Vector2(15, 15));
+}
+
 function TIC() {
-	cls(13)
-	print("HELLO WORLD!", 84, 84)
+	// UPDATE
+	player.update();
+
+	// DRAW
+	map(0, 0, 30, 17, 0, 0);
+	player.draw();
+
+	frame++;
+}
+
+class Player {
+	constructor() {
+		this.setPosition(Vector2.zero());
+		this.lastMovedFrame = -Infinity;
+		this.moveDelay = 16;
+	}
+
+	setPosition(position) {
+		this.position = position;
+		this.displayPosition = this.position.mul(8);
+	}
+
+	update() {
+		const targetDisplayPosition = this.position.mul(8);
+		this.displayPosition = this.displayPosition.moveTo(targetDisplayPosition, 1);
+		this.control();
+	}
+
+	control() {
+		if (this.lastMovedFrame + this.moveDelay <= frame) {
+			if (btn(0) && btn(2)) this.moveUpLeft();
+			else if (btn(1) && btn(3)) this.moveDownRight();
+			else if (btn(2) && btn(1)) this.moveLeftDown();
+			else if (btn(3) && btn(0)) this.moveRightUp();
+			else if (btn(0)) this.moveUp();
+			else if (btn(1)) this.moveDown();
+			else if (btn(2)) this.moveLeft();
+			else if (btn(3)) this.moveRight();
+		}
+	}
+
+	moveUpLeft() {
+		if (this.getNeighbourFlag(0, -1, 0)) {
+			this.moveLeft();
+			this.moveUp();
+		} else {
+			this.moveUp();
+			this.moveLeft();
+		}
+	}
+
+	moveDownRight() {
+		if (this.getNeighbourFlag(0, 1, 0)) {
+			this.moveRight();
+			this.moveDown();
+		} else {
+			this.moveDown();
+			this.moveRight();
+		}
+	}
+
+	moveLeftDown() {
+		if (this.getNeighbourFlag(-1, 0, 0)) {
+			this.moveDown();
+			this.moveLeft();
+		} else {
+			this.moveLeft();
+			this.moveDown();
+		}
+	}
+
+	moveRightUp() {
+		if (this.getNeighbourFlag(1, 0, 0)) {
+			this.moveUp();
+			this.moveRight();
+		} else {
+			this.moveRight();
+			this.moveUp();
+		}
+	}
+
+	moveUp() {
+		if (this.getNeighbourFlag(0, -1, 0)) return;
+		this.position.y--;
+		this.lastMovedFrame = frame;
+	}
+
+	moveDown() {
+		if (this.getNeighbourFlag(0, 1, 0)) return;
+		this.position.y++;
+		this.lastMovedFrame = frame;
+	}
+
+	moveLeft() {
+		if (this.getNeighbourFlag(-1, 0, 0)) return;
+		this.position.x--;
+		this.lastMovedFrame = frame;
+	}
+
+	moveRight() {
+		if (this.getNeighbourFlag(1, 0, 0)) return;
+		this.position.x++;
+		this.lastMovedFrame = frame;
+	}
+
+	getNeighbourFlag(offsetX, offsetY, flag) {
+		let { x, y } = this.position.add(new Vector2(offsetX, offsetY));
+		return fget(mget(x, y), flag);
+	}
+
+	draw() {
+		let { x, y } = this.displayPosition;
+		rect(x, y, 8, 8, 12);
+	}
+}
+
+class Vector2 {
+	static zero() {
+		return new Vector2(0, 0);
+	}
+
+	constructor(x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
+
+	add(vector) {
+		return new Vector2(this.x + vector.x, this.y + vector.y);
+	}
+
+	sub(vector) {
+		return new Vector2(this.x - vector.x, this.y - vector.y);
+	}
+
+	mul(scaler) {
+		return new Vector2(this.x * scaler, this.y * scaler);
+	}
+
+	moveTo(vector, step) {
+		return new Vector2(
+			moveTo(this.x, vector.x, step),
+			moveTo(this.y, vector.y, step));
+	}
+}
+
+function moveTo(start, end, step) {
+	let differance = end - start;
+	differance = Math.sign(differance) * step;
+	return start + differance;
 }
 
 // <WAVES>
